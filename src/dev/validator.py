e5b8548dbd65a3553ec74d55f1787121c44470ce
larnhold <lukas.arnhold@hotmail.com>
2020-12-21 15:08:28 +0100

    validation

--- src/dev/validator.py
+++ src/dev/validator.py
@@ -5,13 +5,26 @@ import time
 
 from sklearn.model_selection import KFold
 
+from classifier.FullTextFastTextModel.FullTextFastTextModel import FullTextFastTextModel
+from classifier.FullTextFastTextModel.validation.FullTextFastTextModelDescOnly import FullTextFastTextModelDescOnly
+from classifier.FullTextFastTextModel.validation.FullTextFastTextModelTitleOnlyl import FullTextFastTextModelTitleOnly
 from classifier.FullTextSvmModel.FullTextSvmModel import FullTextSvmModel
+from classifier.FullTextSvmModel.validation.FullTextSvmModelDescOnly import FullTextSvmModelDescOnly
+from classifier.FullTextSvmModel.validation.FullTextSvmModelTitleOnly import FullTextSvmModelTitleOnly
 from classifier.FullTextTransformerModel.FullTextTransformerModel import FullTextTransformerModel
+from classifier.FullTextTransformerModel.config.TransformerModelConfig import PytorchTransformerConfig
 from entity.Tender import Tender
 
 models = {
+    "FullTextFastTextModel": FullTextFastTextModel(),
+    "FullTextFastTextModelTitleOnly": FullTextFastTextModelTitleOnly(),
+    "FullTextFastTextModelDescOnly": FullTextFastTextModelDescOnly(),
+    "FullTextTransformerModel": FullTextTransformerModel(PytorchTransformerConfig.bert_german_full()),
+    "FullTextTransformerModelTitleOnly": FullTextTransformerModel(PytorchTransformerConfig.bert_german_title_only()),
+    "FullTextTransformerModelDescOnly": FullTextTransformerModel(PytorchTransformerConfig.bert_german_description_only()),
     "FullTextSvmModel": FullTextSvmModel(),
-    #"FullTextTransformerModel": FullTextTransformerModel()
+    "FullTextSvmModelTitleOnly": FullTextSvmModelTitleOnly(),
+    "FullTextSvmModelDescOnly": FullTextSvmModelDescOnly(),
 }
 
 
@@ -26,18 +39,22 @@ class Validator:
     def __init__(self):
         self.start = time.time()
         labelled_tenders = self.get_tenders()
+        #labelled_tenders = labelled_tenders[:1000]
         kfold = KFold(5, True, 1)
 
         # enumerate splits
         iteration = 0
         for train, test in kfold.split(labelled_tenders):
-            print('train: %s, test: %s' % (len(train), len(test)))
             iteration = iteration + 1
+            print('K %s of 5' % iteration)
+            print('train: %s, test: %s' % (len(train), len(test)))
             for name, impl in models.items():
                 impl.create_new_model()
                 impl.train([labelled_tenders[i] for i in train])
                 self.write_result(name, iteration, impl.validate([labelled_tenders[i] for i in test]))
                 pass
+
+            #break
 
     def write_result(self, name, iteration, val_result):
         with open('test_result_' + str(self.start) + '.csv', mode='a') as file:
@@ -47,7 +64,6 @@ class Validator:
                                       val_result.accuracy, val_result.precision, val_result.recall, val_result.f1])
 
     def get_tenders(self):
-
         pos_path = "dev_pos_tenders.json"
         neg_path = "dev_neg_tenders.json"
 
